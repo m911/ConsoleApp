08eb89f00e761fa53f6efa7acab366fe37cf4d1b
m911 <mitaka911@gmail.com>
2023-01-20 06:57:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_SoftUni
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//int a = Convert.ToInt32(Console.ReadLine());
+			//int b = Convert.ToInt32(Console.ReadLine());
+
+			//int area = a * b;
+
+			int result = 0;
+			for (int i = 0; i <= 25;i++)
+			{
+				result += i;
+			}
+
+			Console.WriteLine(false | true);
+
+
+		}
+	}
+}
